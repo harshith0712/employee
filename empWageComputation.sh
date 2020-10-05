93cbf553719b34f941ff6a0b386f7ea6fd5ad889
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-05 17:11:22 +0530

    employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 echo "welcome to the employee wage problem"
-echo "attendance"
-echo "daily wage"
+
+ispresent=1
+randomcheck=$((RANDOM%2))
+
+	if [ $ispresent -eq $randomCheck ];
+	then
+		emprate=20
+		emphrs=8
+		salary=$(($emphrs*$emprate))
+	else
+		salary=0
+	fi
 
