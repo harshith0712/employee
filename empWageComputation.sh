8eedcf1af2682aaa4438a0d52db486617ddcab95
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-05 19:17:11 +0530

    employee wage for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,27 @@
-#!/bin/bash
+#!/bin/bash -x
 
-echo "welcome to the employee wage problem"
+
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+numWorkingDays=20
+
+	for (( day=1; day<=$numWorkingDays; day++ ))
+	do
+		empCheck=$((RANDOM%3))
+
+   	case $empCheck in
+        	 $isFullTime)
+           	 empHrs=8
+           		 ;;
+         	$isPartTime)
+            	empHrs=4
+            	;;
+         	*)
+            	empHrs=0
+            	;;
+   	esac
+		salary=$(($empHrs*$empRatePerHr))
+		totalSalary=$(($totalSalary+$salary))
+	done
 
